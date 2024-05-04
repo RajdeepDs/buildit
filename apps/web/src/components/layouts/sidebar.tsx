d8167af6b72525f665fd0a8b474110718c78c435
RajdeepDs <rajdeepds626@gmail.com>
2024-05-04 13:31:30 +0530

    add settings page with conditional sidebar

--- apps/web/src/components/layouts/sidebar.tsx
+++ apps/web/src/components/layouts/sidebar.tsx
@@ -8,7 +8,7 @@ import { cn } from "@buildit/ui/utils";
 export default function Sidebar(): JSX.Element {
   const pathname = usePathname();
   const { slug } = useParams() as { slug?: string };
-  const tabs = [
+  const dashboardTabs = [
     {
       name: "My Issues",
       href: `/${slug}/my-issues`,
@@ -18,20 +18,68 @@ export default function Sidebar(): JSX.Element {
       href: `/${slug}/settings`,
     },
   ];
+
+  const settingsTabs = [
+    {
+      name: "Profile",
+      href: `/${slug}/settings`,
+    },
+    {
+      name: "Account",
+      href: `/${slug}/settings/account`,
+    },
+    {
+      name: "Notifications",
+      href: `/${slug}/settings/notifications`,
+    },
+    {
+      name: "Security",
+      href: `/${slug}/settings/security`,
+    },
+    {
+      name: "Billing",
+      href: `/${slug}/settings/billing`,
+    },
+  ];
   return (
     <aside className="w-[240px] border">
-      {tabs.map(({ name, href }) => (
-        <Link key={name} href={href}>
-          <div
-            className={cn(
-              "mx-2 mt-2 cursor-pointer rounded-md px-2 py-2",
-              pathname === href && "bg-gray-200 font-semibold",
-            )}
-          >
-            <p className="">{name}</p>
+      {pathname !== `/${slug}/settings` && (
+        <>
+          {dashboardTabs.map(({ name, href }) => (
+            <Link key={name} href={href}>
+              <div
+                className={cn(
+                  "mx-2 mt-2 cursor-pointer rounded-md px-2 py-2",
+                  pathname === href && "bg-gray-200 font-semibold",
+                )}
+              >
+                <p className="">{name}</p>
+              </div>
+            </Link>
+          ))}
+        </>
+      )}
+      {pathname === `/${slug}/settings` && (
+        <>
+          <div className="mx-2 px-2">
+            <Link href={`/${slug}`} className="font-bold">
+              Back
+            </Link>
           </div>
-        </Link>
-      ))}
+          {settingsTabs.map(({ name, href }) => (
+            <Link key={name} href={href}>
+              <div
+                className={cn(
+                  "mx-2 mt-2 cursor-pointer rounded-md px-2 py-2",
+                  pathname === href && "bg-gray-200 font-semibold",
+                )}
+              >
+                <p className="">{name}</p>
+              </div>
+            </Link>
+          ))}
+        </>
+      )}
     </aside>
   );
 }
