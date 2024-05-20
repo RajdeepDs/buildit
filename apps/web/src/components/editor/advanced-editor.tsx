da2cb4e4116844f7e9aaa71eab69e00ad5d47731
RajdeepDs <rajdeepds626@gmail.com>
2024-05-20 21:37:25 +0530

    add tiptap editor by novel

---
+++ apps/web/src/components/editor/advanced-editor.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import {
+  EditorBubble,
+  EditorCommand,
+  EditorCommandEmpty,
+  EditorCommandItem,
+  EditorCommandList,
+  EditorContent,
+  EditorRoot,
+} from "novel";
+import type { JSONContent } from "novel";
+import { handleCommandNavigation, ImageResizer } from "novel/extensions";
+import { handleImageDrop, handleImagePaste } from "novel/plugins";
+
+import { Separator } from "@buildit/ui";
+
+import { defaultExtensions } from "./extensions";
+import { uploadFn } from "./image-upload";
+import { ColorSelector } from "./selectors/color-selector";
+import { LinkSelector } from "./selectors/link-selector";
+import { NodeSelector } from "./selectors/node-selector";
+import { TextButtons } from "./selectors/text-buttons";
+import { slashCommand, suggestionItems } from "./slash-command";
+
+const extensions = [...defaultExtensions, slashCommand];
+
+interface EditorProp {
+  initialValue?: JSONContent;
+  onChange: (value: JSONContent) => void;
+}
+const Editor = ({ initialValue, onChange }: EditorProp) => {
+  const [openNode, setOpenNode] = useState(false);
+  const [openColor, setOpenColor] = useState(false);
+  const [openLink, setOpenLink] = useState(false);
+
+  return (
+    <EditorRoot>
+      <EditorContent
+        className="rounded-xl p-4"
+        {...(initialValue && { initialContent: initialValue })}
+        extensions={extensions}
+        editorProps={{
+          handleDOMEvents: {
+            keydown: (_view, event) => handleCommandNavigation(event),
+          },
+          handlePaste: (view, event) => handleImagePaste(view, event, uploadFn),
+          handleDrop: (view, event, _slice, moved) =>
+            handleImageDrop(view, event, moved, uploadFn),
+          attributes: {
+            class: `prose prose-lg dark:prose-invert prose-headings:font-title font-default focus:outline-none max-w-full`,
+          },
+        }}
+        onUpdate={({ editor }) => {
+          onChange(editor.getJSON());
+        }}
+        slotAfter={<ImageResizer />}
+      >
+        <EditorCommand className="border-muted bg-background z-50 h-auto max-h-[330px] overflow-y-auto rounded-md border px-1 py-2 shadow-md transition-all">
+          <EditorCommandEmpty className="text-muted-foreground px-2">
+            No results
+          </EditorCommandEmpty>
+          <EditorCommandList>
+            {suggestionItems.map((item) => (
+              <EditorCommandItem
+                value={item.title}
+                onCommand={(val) => item.command?.(val)}
+                className={`hover:bg-accent aria-selected:bg-accent flex w-full items-center space-x-2 rounded-md px-2 py-1 text-left text-sm `}
+                key={item.title}
+              >
+                <div className="border-muted bg-background flex h-10 w-10 items-center justify-center rounded-md border">
+                  {item.icon}
+                </div>
+                <div>
+                  <p className="font-medium">{item.title}</p>
+                  <p className="text-muted-foreground text-xs">
+                    {item.description}
+                  </p>
+                </div>
+              </EditorCommandItem>
+            ))}
+          </EditorCommandList>
+        </EditorCommand>
+
+        <EditorBubble
+          tippyOptions={{
+            placement: "top",
+          }}
+          className="border-muted bg-background flex w-fit max-w-[90vw] overflow-hidden rounded-md border shadow-xl"
+        >
+          <Separator orientation="vertical" />
+          <NodeSelector open={openNode} onOpenChange={setOpenNode} />
+          <Separator orientation="vertical" />
+
+          <LinkSelector open={openLink} onOpenChange={setOpenLink} />
+          <Separator orientation="vertical" />
+          <TextButtons />
+          <Separator orientation="vertical" />
+          <ColorSelector open={openColor} onOpenChange={setOpenColor} />
+        </EditorBubble>
+      </EditorContent>
+    </EditorRoot>
+  );
+};
+
+export default Editor;
