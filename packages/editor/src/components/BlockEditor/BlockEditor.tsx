d1a1d95c1c4ada210b5002020cb24466cb629bf9
RajdeepDs <rajdeepds626@gmail.com>
2024-05-26 13:28:29 +0530

    modify BlockEditor to Form Controlled BlockEditor

--- packages/editor/src/components/BlockEditor/BlockEditor.tsx
+++ packages/editor/src/components/BlockEditor/BlockEditor.tsx
@@ -1,30 +1,68 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { EditorContent } from "@tiptap/react";
 
 import "../../styles/index.css";
 
+import { Controller } from "react-hook-form";
+
 import { useBlockEditor } from "../../hooks/useBlockEditor";
 import { LinkMenu, TextMenu } from "../menus";
 
-export const BlockEditor = () => {
+interface BlockEditorProps {
+  control?: any;
+  name?: string;
+}
+
+export const BlockEditor = ({ control, name }: BlockEditorProps) => {
   const menuContainerRef = useRef(null);
   const editorRef = useRef<HTMLDivElement | null>(null);
   const { editor } = useBlockEditor();
+
+  useEffect(() => {
+    editor?.on("blur", ({ editor }) => {
+      console.log(editor.getJSON());
+    });
+  }, [editor]);
+
   if (!editor) return null;
-  return (
-    <div className="flex h-full" ref={menuContainerRef}>
-      <div className="relative flex h-full w-full flex-1 flex-col px-3 py-1">
-        <EditorContent
-          editor={editor}
-          ref={editorRef}
-          className="flex-1 overflow-y-auto"
-        />
-        {/* <ContentItemMenu editor={editor} /> */}
-        <LinkMenu editor={editor} appendTo={menuContainerRef} />
-        <TextMenu editor={editor} />
+
+  if (editor && !control) {
+    return (
+      <div className="flex h-full" ref={menuContainerRef}>
+        <div className="relative flex h-full w-full flex-1 flex-col px-3 py-1">
+          <EditorContent
+            editor={editor}
+            ref={editorRef}
+            className="flex-1 overflow-y-auto"
+          />
+          <LinkMenu editor={editor} appendTo={menuContainerRef} />
+          <TextMenu editor={editor} />
+        </div>
       </div>
-    </div>
+    );
+  }
+
+  return (
+    <Controller
+      name={name || ""}
+      control={control}
+      defaultValue=""
+      render={({ field }) => (
+        <div className="flex h-full" ref={menuContainerRef}>
+          <div className="relative flex h-full w-full flex-1 flex-col px-3 py-1">
+            <EditorContent
+              editor={editor}
+              ref={editorRef}
+              className="flex-1 overflow-y-auto"
+              onBlur={() => field.onChange(editor.getJSON())}
+            />
+            <LinkMenu editor={editor} appendTo={menuContainerRef} />
+            <TextMenu editor={editor} />
+          </div>
+        </div>
+      )}
+    />
   );
 };
--- packages/editor/src/hooks/useBlockEditor.tsx
+++ packages/editor/src/hooks/useBlockEditor.tsx
@@ -7,5 +7,6 @@ export const useBlockEditor = () => {
     autofocus: true,
     extensions: [...ExtensionsKit()],
   });
+
   return { editor };
 };
