1f257a363f8049d564f1910add5e9b90879b60a4
RajdeepDs <rajdeepds626@gmail.com>
2024-06-05 12:34:07 +0530

    modify getIssues

--- apps/web/src/lib/data/issues/get-issues.ts
+++ apps/web/src/lib/data/issues/get-issues.ts
@@ -1,15 +1,36 @@
 "use server";
 
 import { db, eq } from "@buildit/db";
-import { issue } from "@buildit/db/src/schema";
+import { issue, users } from "@buildit/db/src/schema";
+
+import { getSession } from "../get-session";
 
 export const getIssues = async () => {
   try {
+    const session = await getSession();
+    if (!session) return null;
+
+    const workspaceId = await db.query.users.findFirst({
+      where: eq(users.id, session?.id),
+      columns: {},
+      with: {
+        workspaces: {
+          columns: {
+            id: true,
+          },
+        },
+      },
+    });
+
+    if (!workspaceId?.workspaces[0]?.id) return null;
+
     const issues = await db.query.issue.findMany({
+      where: eq(issue.workspaceId, workspaceId.workspaces[0]?.id),
       with: {
         reporter: true,
       },
     });
+
     return issues;
   } catch {
     return null;
