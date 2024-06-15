872b428ebc241fd419b37877ca114a78fdae49e4
RajdeepDs <rajdeepds626@gmail.com>
2024-06-15 10:15:38 +0530

    add prefetch query in dashboard sidebar

--- apps/web/src/app/(dashboard)/[slug]/layout.tsx
+++ apps/web/src/app/(dashboard)/[slug]/layout.tsx
@@ -1,17 +1,39 @@
 import React from "react";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
 import Sidebar from "@/components/layouts/sidebar";
+import { getUser } from "@/lib/data/user/get-user";
+import { getWorkspace } from "@/lib/data/workspace/get-workspace";
 
 export const runtime = "edge";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
-}): JSX.Element {
+  params: { slug: string };
+}): Promise<JSX.Element> {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["user"],
+    queryFn: getUser,
+  });
+  if (params.slug !== "favicon.ico")
+    await queryClient.prefetchQuery({
+      queryKey: ["workspace", { slug: params.slug }],
+      queryFn: async () => getWorkspace({ workspaceSlug: params.slug }),
+    });
+
   return (
     <div className="flex h-dvh">
-      <Sidebar />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Sidebar />
+      </HydrationBoundary>
       <main className="flex w-full flex-grow flex-col overflow-hidden bg-white">
         {children}
       </main>
