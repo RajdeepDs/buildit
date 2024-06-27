5e495f183bb1cf50d0c41ceca67ee98aa72d53f5
RajdeepDs <rajdeepds626@gmail.com>
2024-06-27 19:01:57 +0530

    add page header with sub header

--- apps/web/src/app/(dashboard)/[slug]/my-issues/page.tsx
+++ apps/web/src/app/(dashboard)/[slug]/my-issues/page.tsx
@@ -5,7 +5,7 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 
-import { Button } from "@buildit/ui";
+import { Button, Input } from "@buildit/ui";
 import { Icons } from "@buildit/ui/icons";
 
 import AllIssues from "@/components/issue/all-issues";
@@ -30,10 +30,29 @@ export default async function MyIssuesPage() {
     <div className="h-full">
       <PageHeader title="My issues">
         <Button color="minimal" size={"icon"}>
-          <Icons.panelRight className="text-subtle active:text-emphasis h-4 w-4" />
+          <Icons.horizontalMore className="text-subtle active:text-emphasis h-4 w-4" />
         </Button>
       </PageHeader>
-      <main>
+      <div className="flex items-center justify-between px-4">
+        {/* ToDo: Create an input ui component */}
+        <div className="border-muted flex items-center space-x-2 rounded-md border px-2 py-1">
+          <Icons.search className="text-subtle h-4 w-4" />
+          <input
+            className="flex items-center outline-none placeholder:text-sm"
+            placeholder="Search..."
+          />
+        </div>
+        {/* ToDo: Add a dropdown menu in both the buttons */}
+        <div className="flex space-x-2">
+          <Button color="minimal" size={"icon"}>
+            <Icons.filter className="text-subtle active:text-emphasis h-4 w-4" />
+          </Button>
+          <Button color="minimal" size={"icon"}>
+            <Icons.sliders className="text-subtle active:text-emphasis h-4 w-4" />
+          </Button>
+        </div>
+      </div>
+      <main className="px-4">
         <HydrationBoundary state={dehydrate(queryClient)}>
           <AllIssues />
         </HydrationBoundary>
