c1c507c685c2e01fee08533251cbe377d004c099
RajdeepDs <rajdeepds626@gmail.com>
2024-07-07 10:59:17 +0530

    add empty state for teams issue pages

--- apps/web/src/components/issue/issues-by-team.tsx
+++ apps/web/src/components/issue/issues-by-team.tsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from "react";
 import type { TIssues } from "@/types";
 
 import type { Store } from "@/lib/store/my-issues-store";
+import { CreateIssueModal } from "../modals/create-issue-modal";
+import { Button } from "@buildit/ui";
 
 export default function IssuesByTeam({
   store,
@@ -39,14 +41,29 @@ export default function IssuesByTeam({
   }, [issues, filteredStatus, filteredPriority, store.search]);
 
   return (
-    <div>
-      <ul className="list-none">
-        {filteredIssues?.map((issue) => (
-          <li key={issue.id}>
-            <IssueItem issue={issue} />
-          </li>
-        ))}
-      </ul>
+    <div className="h-full">
+      {issues.length === 0 ? (
+        <div className="flex h-1/2 w-full flex-col items-center justify-center space-y-4 rounded-lg">
+          <div className="flex flex-col items-center">
+            <h1 className="text-default font-cal text-xl">No issues found</h1>
+            <p className="text-subtle text-sm">
+              There aren&apos;t any issues at the moment. Create one to get
+              started!{" "}
+            </p>
+          </div>
+          <CreateIssueModal>
+            <Button StartIcon="plus">Create issue</Button>
+          </CreateIssueModal>
+        </div>
+      ) : (
+        <ul className="list-none">
+          {filteredIssues?.map((issue) => (
+            <li key={issue.id}>
+              <IssueItem issue={issue} />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
