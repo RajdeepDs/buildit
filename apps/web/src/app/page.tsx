afae5626405dc1af66e7b9dd658008cf7e3ddf0d
RajdeepDs <rajdeepds626@gmail.com>
2024-07-07 20:35:41 +0530

    modify homepage

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,34 +1,18 @@
 import { Button } from "@buildit/ui";
 
-import { getSession } from "@/lib/data/get-session";
-import { getWorkspaceSlug } from "@/lib/data/workspace/get-workspace-slug";
-
 export const runtime = "edge";
 
 export default async function Home() {
-  const isSession = await getSession();
-  const workspaceSlug = await getWorkspaceSlug();
-
   return (
-    <main className="">
-      <div className="flex h-full w-full flex-col items-center justify-center">
-        <h1 className="text-6xl font-bold">Welcome to buildit!</h1>
-        <p className="mt-4 text-center text-lg">
-          An open source collaborative project management tool.
+    <main className="mt-10">
+      <div className="flex h-full w-full flex-col items-center justify-center space-y-5">
+        <h1 className="text-5xl font-bold font-cal text-emphasis">
+          Welcome to BuildIt!
+        </h1>
+        <p className="text-2xl font-cal text-subtle">
+          The open-source project development tool.
         </p>
-        {isSession ? (
-          <Button
-            color="secondary"
-            href={`/${workspaceSlug}/my-issues`}
-            className="my-10"
-          >
-            <p>Dashboard</p>
-          </Button>
-        ) : (
-          <Button color="primary" href="/sign-in" className="my-10">
-            Sign In
-          </Button>
-        )}
+        <Button>Coming soon!</Button>
       </div>
     </main>
   );
