81f768b6cde6d9317f010e30fb5882888c7fd154
RajdeepDs <rajdeepds626@gmail.com>
2024-07-23 21:54:27 +0530

    redesign landing page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,42 +1,49 @@
+import HeroSection from "@/components/marketing/hero";
+import JoinCommunity from "@/components/marketing/join-community";
+import NavBar from "@/components/marketing/nav";
+import PoweredBy from "@/components/marketing/powered-by";
+
 import { Button } from "@buildit/ui";
-import Image from "next/image";
 
 export const runtime = "edge";
 
 export default async function Home() {
   return (
-    <main className="container">
-      <nav className="flex items-center justify-between py-2 md:p-2">
-        <div className="flex items-center space-x-2">
-          <Image src="/buildit-logo.svg" alt="logo" width={30} height={30} />
-          <h1 className="font-cal text-strong text-xl">BuildIt</h1>
-        </div>
-        <div className="flex space-x-2">
-          <Button className="hidden" color="secondary">
+    <main className="mx-3 py-3 md:container">
+      <NavBar />
+      <HeroSection />
+      <PoweredBy />
+      <section className="grid items-center justify-between space-y-4 px-7 py-16 md:py-32 lg:grid-cols-2 lg:flex-row">
+        <h1 className="font-cal text-4xl text-strong leading-12 md:text-7xl">
+          Plan, <br className="hidden md:block" /> Execute &{" "}
+          <br className="hidden md:block" /> Deliver.
+        </h1>
+        <p className="font-bold text-soft text-xl tracking-wide md:text-4xl">
+          Easier way to <span className="text-strong">plan</span>, track, and
+          manage projects. Create your{" "}
+          <span className="text-strong">issues</span> using one platform. Use
+          one tool to <span className="text-strong">collaborate</span>, update
+          progress, and achieve <span className="text-strong">goals</span>{" "}
+          seamlessly.
+        </p>
+      </section>
+      <JoinCommunity />
+      <section className="space-y-4 px-7 py-16 lg:flex lg:justify-between lg:py-32">
+        <h1 className="font-cal text-2xl text-strong leading-12 lg:text-6xl">
+          Join us Today! <br /> Get started with BuildIt.
+        </h1>
+        <div className="flex items-center space-x-4">
+          <Button
+            color="secondary"
+            className="md:h-14 md:px-6 md:text-lg"
+            href={"/login"}
+          >
             Login
           </Button>
-          <Button className="hidden md:flex">Coming Soon</Button>
+          <Button className="md:h-14 md:px-6 md:text-lg" href={"/signup"}>
+            Get started free
+          </Button>
         </div>
-      </nav>
-      <section className="mt-28 flex flex-col items-center justify-center space-y-16">
-        <div className="space-y-4 text-center text-strong">
-          <h1 className="font-cal text-3xl lg:text-5xl">
-            Build better, Together with BuildIt
-          </h1>
-          <p className="font-medium text-base text-strong leading-snug md:text-lg">
-            Collaborate seamlessly and manage your projects{" "}
-            <br className="hidden md:flex" /> efficiently with BuildIt's
-            powerful, open-source platform.
-          </p>
-          <Button>Coming Soon</Button>
-        </div>
-        <Image
-          src={"/dashboard.png"}
-          alt="Dashboard"
-          height={516}
-          width={900}
-          className="rounded-lg border shadow-lg"
-        />
       </section>
     </main>
   );
