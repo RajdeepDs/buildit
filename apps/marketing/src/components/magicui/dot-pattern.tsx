c3108933cccc354d62c7f2e76e8f75ecda70af40
Rajdeep Das <rajdeepds626@gmail.com>
2024-09-04 21:39:14 +0530

    fix(marketing): dot-patterns are not repetative on entire page

--- apps/marketing/src/components/magicui/dot-pattern.tsx
+++ apps/marketing/src/components/magicui/dot-pattern.tsx
@@ -1,5 +1,7 @@
 import { useId } from 'react'
 
+import type { ReactNode } from 'react'
+
 import { cn } from '@/lib/utils'
 
 interface DotPatternProps {
@@ -12,6 +14,7 @@ interface DotPatternProps {
   cr?: any
   className?: string
   [key: string]: any
+  children?: ReactNode
 }
 /**
  * DotPattern
@@ -24,6 +27,7 @@ interface DotPatternProps {
  * @param props.cy
  * @param props.cr
  * @param props.className
+ * @param props.children
  * @returns JSX.Element
  */
 export function DotPattern({
@@ -31,38 +35,51 @@ export function DotPattern({
   height = 16,
   x = 0,
   y = 0,
-  cx = 1,
-  cy = 1,
+  cx = 2,
+  cy = 2,
   cr = 1,
   className,
+  children,
   ...props
 }: DotPatternProps) {
   const id = useId()
 
   return (
-    <svg
-      aria-hidden='true'
-      className={cn(
-        'pointer-events-none absolute inset-0 h-full w-full fill-neutral-400/80',
-        className,
-      )}
-      {...props}
-    >
-      <defs>
-        <pattern
-          id={id}
-          width={width}
-          height={height}
-          patternUnits='userSpaceOnUse'
-          patternContentUnits='userSpaceOnUse'
-          x={x}
-          y={y}
-        >
-          <circle id='pattern-circle' cx={cx} cy={cy} r={cr} />
-        </pattern>
-      </defs>
-      <rect width='100%' height='100%' strokeWidth={0} fill={`url(#${id})`} />
-    </svg>
+    <>
+      <div className='-z-50 fixed top-0 left-0'>
+        <div className='sticky top-0 left-0 h-screen w-screen overflow-hidden'>
+          <svg
+            aria-hidden='true'
+            className={cn(
+              'pointer-events-none absolute inset-0 h-full w-full fill-neutral-400/80',
+              className,
+            )}
+            {...props}
+          >
+            <defs>
+              <pattern
+                id={id}
+                width={width}
+                height={height}
+                patternUnits='userSpaceOnUse'
+                patternContentUnits='userSpaceOnUse'
+                x={x}
+                y={y}
+              >
+                <circle id='pattern-circle' cx={cx} cy={cy} r={cr} />
+              </pattern>
+            </defs>
+            <rect
+              width='100%'
+              height='100%'
+              strokeWidth={0}
+              fill={`url(#${id})`}
+            />
+          </svg>
+        </div>
+      </div>
+      {children}
+    </>
   )
 }
 
