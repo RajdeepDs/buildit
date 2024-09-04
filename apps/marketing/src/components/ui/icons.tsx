19a7c48a7078be4737c25b3d411e67e50e98f3aa
Rajdeep Das <rajdeepds626@gmail.com>
2024-09-04 21:16:39 +0530

    feat(marketing): add header and hero section

---
+++ apps/marketing/src/components/ui/icons.tsx
@@ -0,0 +1,10 @@
+import type { LucideIcon } from 'lucide-react'
+
+import { ChevronRight, Star } from 'lucide-react'
+
+export type Icon = LucideIcon
+
+export const Icons = {
+  chevronRight: ChevronRight,
+  star: Star,
+}
