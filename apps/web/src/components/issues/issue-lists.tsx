95669c2c94b175922e8ed05a54ff94e99fd99e1c
Rajdeep Das <rajdeepds626@gmail.com>
2024-09-24 22:35:36 +0530

    feat(web): add empty state for my issues

--- apps/web/src/components/issues/issue-lists.tsx
+++ apps/web/src/components/issues/issue-lists.tsx
@@ -17,14 +17,28 @@ export default function IssueList(): JSX.Element {
 
   return (
     <>
-      {allIssues?.map((issue, index) => (
-        <IssueItem
-          key={issue.id}
-          issue={issue}
-          isFirst={index === 0}
-          isLast={index === allIssues.length - 1}
-        />
-      ))}
+      {allIssues?.length === 0 ? (
+        <div className='flex h-1/2 w-full flex-col items-center justify-center space-y-4 rounded-lg'>
+          <div className='flex flex-col items-center'>
+            <h1 className='font-cal text-strong text-xl'>No issues found</h1>
+            <p className='text-sm text-sub'>
+              There aren&apos;t any issues at the moment. Create one to get
+              started!{' '}
+            </p>
+          </div>
+        </div>
+      ) : (
+        <>
+          {allIssues?.map((issue, index) => (
+            <IssueItem
+              key={issue.id}
+              issue={issue}
+              isFirst={index === 0}
+              isLast={index === allIssues.length - 1}
+            />
+          ))}
+        </>
+      )}
     </>
   )
 }
