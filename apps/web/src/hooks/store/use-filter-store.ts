8dec22c686150557c94eabdbce72483653000df3
Rajdeep Das <rajdeepds626@gmail.com>
2024-10-07 21:02:19 +0530

    feat(web): create an optimized and reusable filter store

---
+++ apps/web/src/hooks/store/use-filter-store.ts
@@ -0,0 +1,18 @@
+import { usePathname } from 'next/navigation'
+
+import type { FilterStore } from '@/lib/store/filter-store'
+
+import { createFilterStore } from '@/lib/store/filter-store'
+
+export const useFilterStore = (): FilterStore => {
+  let pathname = usePathname()
+  if (pathname === '/') {
+    pathname = '/my-issues'
+  }
+  const useIssuesFilterStore = createFilterStore(pathname)
+
+  const and = useIssuesFilterStore((state) => state.and)
+  const setFilter = useIssuesFilterStore((state) => state.setFilter)
+
+  return { and, setFilter }
+}
---
+++ apps/web/src/lib/store/filter-store.ts
@@ -0,0 +1,56 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+type Operator =
+  | 'eq'
+  | 'ne'
+  | 'gt'
+  | 'gte'
+  | 'lt'
+  | 'lte'
+  | 'in'
+  | 'nin'
+  | 'and'
+  | 'or'
+
+type FilterCondition = Record<
+  string,
+  {
+    [key in Operator]?: any
+  }
+>
+
+interface FilterQuery {
+  [key: string]: FilterCondition | FilterQuery
+}
+
+export interface FilterStore {
+  and: FilterQuery
+  setFilter: (type: string, operator: Operator, value: any) => void
+}
+
+export const createFilterStore = (pathname: string) =>
+  create<FilterStore>()(
+    persist(
+      (set) => ({
+        and: {},
+        setFilter: (type, operator, value) => {
+          set((state) => {
+            return {
+              ...state.and, // keep the existing filters and add the new one
+              and: {
+                [type]: {
+                  [operator]: value,
+                },
+              },
+            }
+          })
+        },
+      }),
+      {
+        name: `filter${pathname}`,
+      },
+    ),
+  )
+
+// export const useMyIssuesFilterStore = createFilterStore('my-issues')
