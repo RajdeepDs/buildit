70dd90585e7b5dd51924c8a8f6265216166b7359
Rajdeep Das <rajdeepds626@gmail.com>
2024-10-27 20:40:16 +0530

    feat(web): add team lists and style team item component

--- apps/web/src/app/(dashboard)/teams/page-client.tsx
+++ apps/web/src/app/(dashboard)/teams/page-client.tsx
@@ -3,6 +3,7 @@
 import { Button } from '@buildit/ui/button'
 
 import Header from '@/components/layout/header'
+import TeamList from '@/components/teams/team-list'
 import { Icons } from '@/components/ui/icons'
 
 /**
@@ -15,12 +16,17 @@ export default function TeamsClientPage(): JSX.Element {
       <div className='h-full flex flex-col gap-2'>
         <Header>
           <div className='flex items-center gap-2'>
+            {/* TODO: Add new team modal to create a new team */}
             <Button variant={'secondary'} size={'sm'} className='h-7'>
               <Icons.plus className='size-4 text-sub mr-1' />
               Add team
             </Button>
           </div>
         </Header>
+        {/* TODO: Add Filter menu and display settings options */}
+        <div className='h-full w-full'>
+          <TeamList />
+        </div>
       </div>
     </>
   )
