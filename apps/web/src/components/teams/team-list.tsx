e85836cef77ffc109116e06d15cfeac6d87b7f13
Rajdeep Das <rajdeepds626@gmail.com>
2024-11-19 19:22:43 +0530

    feat(web): add empty-state for teams pages

--- apps/web/src/components/teams/team-list.tsx
+++ apps/web/src/components/teams/team-list.tsx
@@ -1,15 +1,29 @@
 'use client'
 
-import { api } from '@/lib/trpc/react'
+import { toast } from '@buildit/ui/toast'
 
-import TeamItem from './team-item'
+import TeamItem from '@/components/teams/team-item'
+import EmptyState from '@/components/ui/empty-state'
+import { api } from '@/lib/trpc/react'
 
 /**
  * The team list component. Lists all the teams.
  * @returns JSX.Element
  */
 export default function TeamList(): JSX.Element {
-  const { data: allTeams } = api.team.get_teams.useQuery()
+  const { data: allTeams, error } = api.team.get_teams.useQuery()
+
+  if (allTeams?.length === 0) {
+    return <EmptyState id='teams' />
+  }
+
+  if (error) {
+    toast({
+      title: 'Error',
+      description: 'Failed to fetch teams',
+      variant: 'destructive',
+    })
+  }
   return (
     <div>{allTeams?.map((team) => <TeamItem key={team.id} team={team} />)}</div>
   )
