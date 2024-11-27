c161beb9722dfe6764bcc099fb6feafb9a935a0d
Rajdeep Das <rajdeepds626@gmail.com>
2024-11-27 11:21:37 +0530

    feat(web): add issue-menu dropdown component

--- apps/web/src/hooks/mutations/use-create-project.tsx
+++ apps/web/src/hooks/mutations/use-create-project.tsx
@@ -15,10 +15,8 @@ export function useCreateProject() {
 
   return api.project.create_project.useMutation({
     onMutate: async (newProject) => {
-      // Cancel ongoing queries to avoid conflicts
       await queryClient.cancelQueries({ queryKey: [['project']] })
 
-      // Optimistically add the new project to cached queries
       const queryKeys = queryClient
         .getQueriesData({
           queryKey: [['project']],
@@ -38,14 +36,12 @@ export function useCreateProject() {
       return { previousProjectData }
     },
     onError: (error, variables, context) => {
-      // Revert cache to previous state if mutation fails
       if (context?.previousProjectData) {
         Object.entries(context.previousProjectData).forEach(([key, data]) => {
           queryClient.setQueryData(JSON.parse(key), data)
         })
       }
 
-      // Show a detailed error toast
       toast({
         variant: 'destructive',
         title: 'Error creating project!',
@@ -53,13 +49,11 @@ export function useCreateProject() {
       })
     },
     onSuccess: async (data) => {
-      // Show a success toast
       toast({
         title: 'Project created',
         description: data.message,
       })
 
-      // Invalidate all queries related to projects to refresh data
       await queryClient.invalidateQueries({ queryKey: [['project']] })
     },
   })
